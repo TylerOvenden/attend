1205da3f911c230b950a2cba4bf3d26d96fb7b4a
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-26 11:44:26 -0400

    fv

---
+++ attendance/src/attendance/Student.java
@@ -0,0 +1,46 @@
+package attendance;
+
+public class Student implements Attendee {
+
+	@Override
+	public boolean isPresent() {
+		if ()
+	}
+
+	@Override
+	public void setPresent(boolean present) {
+		// TODO Auto-generated method stub
+		return present.isPresent;
+	}
+
+	@Override
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return ;
+	}
+
+	@Override
+	public String getLastName() {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public boolean mathces(String first, String last) {
+		// TODO Auto-generated method stub
+		return;
+	}
+
+	@Override
+	public boolean matches(String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getReportString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
