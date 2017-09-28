d66126a939ca2e99599565de5f86f4ac3c91243b
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-28 11:36:07 -0400

    scsd

---
+++ attendance/src/chatbotProject/ChatbotMain.java
@@ -0,0 +1,13 @@
+package chatbotProject;
+
+public class ChatbotMain {
+
+	
+	public static Chatbot chatbot = new Chatbot();
+	
+	public static void main(String[] args) {
+		chatbot.startTalking();
+
+	}
+
+}
