4b4c28a10031aaea8deca73abd25ca9cd70112cc
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-28 11:45:14 -0400

    rd

--- attendance/src/chatbotProject/Chatbot.java
+++ attendance/src/chatbotProject/Chatbot.java
@@ -1,5 +1,30 @@
 package chatbotProject;
 
 public class Chatbot {
-
+	private String userName;
+	private Topic tyler;
+	private boolean chatting;
+	public Chatbot()
+	{
+		ben = new ChatbotTyler();
+		userName = "unknown user";
+		chatting = true;
+	}
+	public void startTalking() {
+		ChatbotMain.print("welcome to our chatbot! Your nama?");
+		userName = ChatbotMain.getInput();
+		while (chatting) {
+			ChatbotMain.print("What do you want to talk about?")
+			String response = ChatbotMain.getInput();
+			if (tyler.isTriggered(response)) {
+				chatting = false;
+				tyler.startChatting();
+			} else {
+				ChatbotMain.print("I'm sorry. I don't understand");
+				
+				
+			}
+		}
+	}
+	
 }
