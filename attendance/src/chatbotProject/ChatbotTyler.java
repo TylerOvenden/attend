df18abd59707a7a9767f5015e7d71bd818bce28f
TylerOvenden <tovenden4386@bths.edu>
2017-10-03 11:37:47 -0400

    Update ChatbotTyler.java

--- attendance/src/chatbotProject/ChatbotTyler.java
+++ attendance/src/chatbotProject/ChatbotTyler.java
@@ -1,116 +1,78 @@
-package chatbotProject;
+package chatbotproject;
 
-import java.util.Scanner;
-
-public class ChatbotTyler {
-
+public class ChatbotTyler implements Topic {
+	private String[] keywords;
+	private String goodbyewords;
+	private String secretWord;
+	private String[] shooters = {"fps", "first person shooter", "shooting", "1st person", "3rd person", "third person"};
+	private String[] platform = {"platform", "platforming", "sidescroller", "platformer", "side scrolling"};
+	private String[] Rshooters = {"Doom", "Quake", "Call of Duty", "Halo", "Gears of War"};
+	private String[] Rplatformers = {"Super Mario Bros","Sonic the Hedgehog", "Rayman", "Super Meat Boy", "Cuphead","Duck Game"};
+	private String[] negativeWord = {"hate", "don't like", "don't love", "horrible"};
+	private boolean chatting; 
+	private boolean isShoot = false;
+	private boolean isPlat = false;
 	public ChatbotTyler() {
-		// TODO Auto-generated constructor stub
+		goodbyewords = "jeff";
+		secretWord = "doge";
+		
 	}
-	private static Scanner inputSource = new Scanner(System.in);
-
-
-
-	/**
-	 * This method return the index of keyword in the searchString 
-	 * where keyword is isolated and has noNegations. It returns -1 if the
-	 * keyword is not found
-	 * @return
-	 */
-	public static int findKeyword(String searchString, String keyword, int startPsn) {
-		//make lowercase
-		searchString = searchString.toLowerCase();
-		keyword = keyword.toLowerCase();
-		//find the first position after the startPsn
-		int psn = searchString.indexOf(keyword, startPsn);
-		//keep searching until keyword is found (noNegations and isolated)
-		while(psn >= 0) {
-			 
-			if (keywordIsIsolated(psn, keyword, searchString) && noNegations(searchString, psn)) {
-				return psn;
-			} else {
-				psn = searchString.indexOf(keyword, psn+1);
+	
+	public boolean isTriggered(String response) {
+		for(int i = 0; i < Rplatformers.length;i++) {
+			if(ChatbotMain.findKeyword(response, rplatformers[i], 0) >= 0) {
+					isPlat = true;
+					return true;
+				 }
 			}
-		}
-		return -1;
-	}
-
-	public static boolean keywordIsIsolated(int psn, String keyword, String s){
-		if (s.substring(psn + keyword.length()+1).equals(" "))
-		{
-			return true;
-		}
-		else if(s.substring(psn + keyword.length()+1).compareTo("a")<0) {
-			return true;
-			
-		}	return false;
-	}
-
-	public static boolean noNegations(String s, int psn){
-		if(s.substring(psn +	)
-			
-
-	}
-	public static String getInput(){
-		return inputSource.nextLine();
-	}
-
-	public static void print(String s){
-		multiLinePrint(s);
-	}
-
-	public static void multiLinePrint(String s){
-		String printString = "";
-		int cutoff = 25;
-		//this while loop last as long as there are words left in the original String
-		while(s.length() > 0){
-
-			String currentCut = "";
-			String nextWord = "";
-
-			//while the current cut is still less than the line length 
-			//AND there are still words left to add
-			while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
-
-				//add the next word
-				currentCut += nextWord;
-
-				//remove the word that was added from the original String
-				s = s.substring(nextWord.length());
-
-				//identify the following word, exclude the space
-				int endOfWord = s.indexOf(" ");
-
-				//if there are no more spaces, this is the last word, so add the whole thing
-				if(endOfWord == -1) {
-					endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
+		for(int i = 0; i < Rshooters.length;i++) {
+			if(ChatbotMain.findKeyword(response, rshooters[i], 0) >= 0) {
+					isShoot = true;
+					return true;
 				}
-
-				//the next word should include the space
-				nextWord = s.substring(0,endOfWord+1);
 			}
-
-			printString +=currentCut+"\n";
-
+				return false;
 		}
-		System.out.print(printString);
-	}
-
-	public static int getIntegerInput() {
-		print("Please enter an integer.");
-		String integerString = getInput();
-		boolean isInteger = false;
-		int value = 0;
-		while(!isInteger){
-			try{
-				value = Integer.parseInt(integerString);
-				//will not continue if an error above is thrown
-				isInteger = true;//exits loop if entry is valid
-			}catch(NumberFormatException e){
-				print("You must enter an integer. You better try again.");
-				integerString = getInput();
+	  
+	
+	public void startChatting(String response) {
+		if (isShoot) {
+			ChatbotMain.print("I too like shooters, is there any particular shooter game you're a big fan of?");
 			}
+		if (isPlat)
+			{	
+		 	ChatbotMain.print("I too like platformers, is there any particular platformer game you're a big fan of?");
+			}
+		chatting = true;
+		while (chatting) {
+
+			 response = ChatbotMain.getInput();
+			if(isShoot) {
+				for(int i = 0; i < Rshooters.length; i++)
+				{
+					if(Rshooters[i].contains(response))
+					{
+						
+						
+					}
+					
+					
+					
+				}	
+			if (ChatbotMain.findKeyword(response,"hate",0) || ChatbotMain.findKeyword(response,"don't like",0) || ChatbotMain.findKeyword(response,"do not like",0)) {
+			ChatbotMain.print("Hey, I didn't ask for a game you don't like, please tell me a game you do like!");	
+			}}
+			if (ChatbotMain.)
+			if (ChatbotMain.findKeyword(response,goodbyewords, 0)>=0) {
+				chatting = false;
+				ChatbotMain.chatbot.startTalking();
+			}else if (ChatbotMain.findKeyword(response, secretWord, 0) >=0) {
+				ChatbotMain.print("Cool! You guessed my favorite thing ever. We are friends now.");		
+			}else {
+				
+				ChatbotMain.print("what");
+			}
+			
 		}
-		return value;
 	}
 }
